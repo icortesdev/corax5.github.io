255a928b9095f6bc6bf53d23fbcae948199dabb7
Irian Cortés <icortesdev@proton.me>
2023-07-24 18:48:20 +0200

    update dark-mode

---
+++ scripts/slider.dark.js
@@ -0,0 +1,14 @@
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'dark' : 'light';
+const sliderDark = document.getElementById('sliderDark');
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+sliderDark.addEventListener('click', ()  => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+});
+
+setTheme(localStorage.getItem('theme') || preferedColorScheme);
