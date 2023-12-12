cb7288bf10ea0edbc0ee266c5ec8173dca02a61c
Irian Cortés <icortesdev@proton.me>
2023-12-12 18:58:28 +0100

    tech & css

--- scripts/data-tilt.js
+++ scripts/data-tilt.js
@@ -3,6 +3,6 @@ const tiltElements = document.querySelectorAll('[data-tilt]');
 // Itera sobre los elementos y aplica los efectos de inclinación
 tiltElements.forEach((element) => {
   VanillaTilt.init(element, {
-    reverse: true, // Establece esta opción en `true` para invertir la inclinación
+    reverse: true,
   });
 });
--- scripts/data.js
+++ scripts/data.js
@@ -2,39 +2,30 @@ var db = {
    projects: [],
    skills: [
       {
-         "name": "HTML",
-         "description": "HTML es el lenguaje de marcado estándar utilizado para crear la estructura y el contenido de las páginas web. Se utiliza para definir la estructura de los elementos y su presentación básica."
+         "name": "HTML"
       },
       {
-         "name": "CSS",
-         "description": "CSS es un lenguaje utilizado para describir la apariencia y el formato de un documento HTML. Se utiliza para definir el estilo, diseño y presentación visual de los elementos en una página web."
+         "name": "CSS"
       },
       {
-         "name": "JavaScript",
-         "description": "JavaScript es un lenguaje de programación interpretado que se utiliza principalmente para crear interactividad en las páginas web. Permite agregar comportamiento dinámico a los elementos de una página y manipular su contenido en tiempo real."
+         "name": "JavaScript"
       },
       {
-         "name": "GIT",
-         "description": "Git es un sistema de control de versiones distribuido utilizado para el seguimiento de cambios en proyectos de software. Permite a los desarrolladores colaborar, realizar un seguimiento de las modificaciones y fusionar el trabajo de múltiples personas en un proyecto."
+         "name": "GIT"
       },
       {
-         "name": "Node",
-         "description": "Node.js es un entorno de tiempo de ejecución de JavaScript basado en el motor V8 de Google Chrome. Permite ejecutar JavaScript en el servidor y facilita la creación de aplicaciones web escalables y de alto rendimiento."
+         "name": "Node"
       },
       {
-         "name": "React",
-         "description": "React es una biblioteca de JavaScript utilizada para construir interfaces de usuario interactivas y reutilizables. Se utiliza para crear componentes web reactivos y permite construir aplicaciones de una sola página (SPA) de manera eficiente."
+         "name": "React"
       },
       {
-         "name": "MongoDB",
-         "description": "MongoDB es una base de datos NoSQL orientada a documentos. Almacena datos en formato BSON (Binary JSON) y se caracteriza por ser flexible y escalable. Es ampliamente utilizado en el desarrollo de aplicaciones web modernas."
+         "name": "MongoDB"
       },
       {
-         "name": "NextJS",
-         "description": "Next.js es un framework de desarrollo web de React que permite construir aplicaciones web rápidas y escalables. Proporciona renderizado híbrido, enrutamiento sencillo y recarga en caliente para una experiencia de desarrollo eficiente."
+         "name": "NextJS"
+      }, {
+         "name": "WordPress"
       },
-
-
    ],
-
 };
